de94d73652869e0745aad60b66289068e250cb60
SprachMensch <Sprachmensch@gmx.de>
2022-06-08 11:34:58 +0200

    update

---
+++ arch_wiki.ps1
@@ -0,0 +1,16 @@
+# 
+# https://mirror.pseudoform.org/community/os/x86_64/arch-wiki-docs-20220525-1-any.pkg.tar.zst
+#
+
+$location="C:\tempPS\usr\share\doc\arch-wiki\html\en\"
+
+$entries=(Get-ChildItem -Path $location -File *.html)
+$entry=($entries.Name | Out-Gridview -Title "Arch Wiki Offline Browser" -passthru )
+
+if ($null -ne $entry ){
+    $entry
+    Start-Process "$location$entry"
+}else{
+    Write-Host "Nothing was selected!"
+}
+  
