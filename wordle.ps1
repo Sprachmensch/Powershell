6e4b1e660cbbcbc8473e25d0c2121cad39becea5
SprachMensch <Sprachmensch@gmx.de>
2022-06-09 10:09:34 +0200

    update

--- wordle.ps1
+++ wordle.ps1
@@ -1,35 +1,101 @@
 <#
 
 to do
--show multiple occurances
--alphabet -> show "used" letters
+- show multiple occurances
+- alphabet -> show "used" letters
+- refactoring
 
+    Colors
+    Black | DarkBlue | DarkGreen | DarkCyan | DarkRed | DarkMagenta | DarkYellow | Gray | DarkGray | Blue |     
+    Green | Cyan | Red | Magenta | Yellow | White
+    
 #>
-#Write-Host "HOT" -NoNewline -Backgroundcolor Green -ForegroundColor Black
-#Write-Host "E" -NoNewline -Backgroundcolor Red -ForegroundColor Black 
-
-Clear-Host
-
-Write-Host "               " -Backgroundcolor Green -ForegroundColor Black
-Write-Host "  W O R D L E  " -Backgroundcolor Green -ForegroundColor Black
-Write-Host "               " -Backgroundcolor Green -ForegroundColor Black
-Write-Host "" 
-Write-Host " X X X X X " -Backgroundcolor Green -ForegroundColor Black
 
 $goal = Get-Content -Path ./words.txt | Get-Random
 $used=""
 $alphabet="abcdefghijklmnopqrstvuwxyz".ToUpper()
 
-$counter=1
-#$goal="HOTEL"
-while($true)
-{
-    $guess=Read-Host -Prompt "Your guess ($counter/5)"
-    Clear-Host
+$counter=4
+$goal="hotel".ToUpper()
+
+function Draw-Header {    
     Write-Host "               " -Backgroundcolor Green -ForegroundColor Black
     Write-Host "  W O R D L E  " -Backgroundcolor Green -ForegroundColor Black
     Write-Host "               " -Backgroundcolor Green -ForegroundColor Black
-    Write-Host "" 
+    Write-Host ""   
+}
+
+function Draw-Alphabet {
+    for ($i=0;$i -le $alphabet.Length;$i++)
+    {
+        Write-Host " " -NoNewline
+        if($used.Contains($alphabet[$i]))
+        {
+            if($goal.Contains($alphabet[$i]))
+            {
+                Write-Host $alphabet[$i] -NoNewline -ForegroundColor Yellow
+            }else{
+                Write-Host $alphabet[$i] -NoNewline -ForegroundColor DarkGray 
+            }
+        }else{
+            Write-Host $alphabet[$i] -NoNewline -ForegroundColor Gray
+        }
+
+        if(($i -eq 6 ) -or( $i -eq 13)-or( $i -eq 20))
+        { 
+            Write-Host "" 
+            if($i -eq 20 )
+            {
+                     Write-Host "  " -NoNewline
+            }
+        }
+        
+    }
+    
+}
+
+function Draw-Goal {
+    param (
+        [Parameter(Mandatory=$true)][bool]$won
+    )
+    $msg=""
+    $color="White"
+
+    if($won)
+    {
+        $msg="    YOU WON!   "
+        $color="Green"
+    }else{
+        $msg="    YOU LOSE   "
+        $color="Red"
+    }
+
+    #Write-Host "               " -Backgroundcolor $color -ForegroundColor Black
+    Write-Host $msg -Backgroundcolor $color -ForegroundColor Black
+    #Write-Host "               " -Backgroundcolor $color -ForegroundColor Black
+    
+    if($won -ne $true){
+        Write-Host " WORD WAS $goal    "
+    }
+}
+
+Clear-Host
+Draw-Header
+
+Write-Host " X  X  X  X  X " -Backgroundcolor Green -ForegroundColor Black
+Write-Host ""
+Draw-Alphabet
+Write-Host ""
+Write-Host ""
+
+
+while($true)
+{
+    Write-Host "Your guess ($counter/5):"
+    $guess=Read-Host
+    Clear-Host
+    Draw-Header
+    
     if($guess -eq "q")
     {
         break
@@ -56,49 +122,24 @@ while($true)
        # Write-Host " " -NoNewline
     }
 
-    <#
-    Colors
-    Black | DarkBlue | DarkGreen | DarkCyan | DarkRed | DarkMagenta | DarkYellow | Gray | DarkGray | Blue |     
-    Green | Cyan | Red | Magenta | Yellow | White
-    #>
-
     Write-Host ""
     Write-Host ""
-    for ($i=0;$i -le $alphabet.Length;$i++)
-    {
-        Write-Host " " -NoNewline
-        if($used.Contains($alphabet[$i]))
-        {
-            if($goal.Contains($alphabet[$i]))
-            {
-                Write-Host $alphabet[$i] -NoNewline -ForegroundColor Yellow
-            }else{
-                Write-Host $alphabet[$i] -NoNewline -ForegroundColor DarkGray 
-            }
-        }else{
-            Write-Host $alphabet[$i] -NoNewline -ForegroundColor Gray
-        }
 
-        if(($i -eq 8 ) -or( $i -eq 17))
-        {
-            Write-Host "" 
-        }
-        
-    }
+    Draw-Alphabet
+  
     Write-Host ""
     Write-Host ""
 
     # check win
     if($goal -eq $guess)
     {
-        Write-Host "YOU WON!" -Backgroundcolor Green -ForegroundColor Black
+        Draw-Goal -won $true
         break
     }
     # check win
     if($counter -ge 5)
     {
-        Write-Host "YOU LOSE" -Backgroundcolor Red -ForegroundColor Black
-        Write-Host " "$goal" " -Backgroundcolor Red -ForegroundColor Black
+        Draw-Goal -won $false
         break
     }
     $counter++
