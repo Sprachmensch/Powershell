617e36b56b2f70414207311b3c23d6a348871939
SprachMensch <Sprachmensch@gmx.de>
2022-06-10 12:30:40 +0200

    usb_tethering was needed

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Collection my of Powershell script/snippets
 - [arch_wiki.ps1](https://github.com/Sprachmensch/Powershell/blob/main/arch_wiki.ps1) search offline in the Arch wiki, absolutly not inspired by Distrowatch...
 - [wordle.ps1](https://github.com/Sprachmensch/Powershell/blob/main/wordle.ps1) small cli wordle clone
 - [pw_gen.ps1](https://github.com/Sprachmensch/Powershell/blob/main/pw_gen.ps1) quick'n'dirty passwort generator
+- [usb_tethering.ps1](https://github.com/Sprachmensch/Powershell/blob/main/usb_tethering.ps1) enable usb tethering via abd
 - ...
 
 ``` Powershell
---
+++ usb_tethering.ps1
@@ -0,0 +1,53 @@
+<#
+    Waits for the device to connect and afterwards starts the USB Tethering
+
+    -> Submit the PIN via the cli !
+
+#>
+
+Param(
+    [Parameter(Mandatory=$True)]
+    [string]$SmartphonePIN
+)
+
+Set-Location C:\Users\AnalogPresent\Desktop\TempADB\
+$waitTime=10    # wait for the "charge screen" to fade out
+
+for ($i=0;$i -lt $waitTime;$i++)
+{
+    Write-Host "Waiting..."
+    Start-Sleep -Seconds 1
+}
+
+Write-Host "Check for Device:"
+$checkDevice=(./adb.exe devices)
+
+# check
+$checkDevice
+if($checkDevice -match "AQH7N18105004832")
+{
+    Write-Host "Found Device"
+}else{
+    Write-Host "Waiting for Device.."
+    Start-Sleep -Seconds 2
+}
+
+<#
+    Turns the screen and and enters the PIN
+#>
+Write-Host "Connected via ADB -> send commands!"
+./adb.exe shell input keyevent 26
+./adb.exe shell input text $SmartphonePIN
+./adb.exe shell am start -n com.android.settings/.TetherSettings
+./adb.exe shell input keyevent 20
+./adb.exe shell input keyevent 20
+./adb.exe shell input keyevent 20
+./adb.exe shell input keyevent 66
+
+Start-Sleep -Seconds 2
+./adb.exe shell input keyevent 4
+./adb.exe shell input keyevent 26
+Write-Host "USB Tethering is now enabled"
+
+./adb.exe kill-server
+Write-Host "Done"
